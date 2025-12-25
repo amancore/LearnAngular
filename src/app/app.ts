4d4d1bbecf5fb3f1712bcc86d0c24c1aac890d7a
Amancore__ <aman8804623822@gmail.com>
2025-12-25 18:47:27 +0530

    added the signals computed, update, set

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,33 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, computed, effect } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   // protected readonly title = signal('LearnAngular');
-	text = ''
-	email: String = ''
-	getEmail(val: String) {
-		this.email = val;
-	}
-	display=true
-	items = ['a','c','d','e']
+  // text = ''
+  // email: String = ''
+  // getEmail(val: String) {
+  // 	this.email = val;
+  // }
+  // display=true
+  // items = ['a','c','d','e']
+
+  count = signal(0);
+
+  doubleCount = computed(() => this.count() * 2);
+
+  constructor() {
+    effect(() => {
+      console.log('Count:', this.count());
+    });
+  }
+
+  inc() {
+    this.count.update((v) => v + 5);
+  }
 }
