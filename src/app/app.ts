06a6710f59cdb3b65bd5991457a7c092891673bf
Amancore__ <aman8804623822@gmail.com>
2025-12-25 18:01:13 +0530

    template reference variable with form handling

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,10 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('LearnAngular');
+  // protected readonly title = signal('LearnAngular');
+	text = ''
+	email: String = ''
+	getEmail(val: String) {
+		this.email = val;
+	}
 }
